069dab9ff70daa16e5f75b78d0bf8117844e9177
EC2 Default User <ec2-user@ip-172-31-41-80.ap-northeast-1.compute.internal>
2019-11-29 07:40:51 +0000

    “restaurant”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
-  
+  config.action_mailer.default_url_options = { host: "https://pure-sierra-31232.herokuapp.com" }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
