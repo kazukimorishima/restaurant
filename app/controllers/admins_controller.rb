e00ee46220a44e07f556dc65dbfcf9c01c910208
EC2 Default User <ec2-user@ip-172-31-41-80.ap-northeast-1.compute.internal>
2019-12-04 13:21:32 +0000

    “restaurant”

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,13 @@
 class AdminsController < ApplicationController
+  before_action :basic_auth, only: [:top]
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+    
   def top
   end
 end
